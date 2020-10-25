b3ae47d41f3c762eb78a20291d5416d6e36444d1
EricShi1255 <eshi20@stuy.edu>
2020-10-24 22:02:56 -0400

    function 1 stuff

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,6 +2,9 @@ public class ArrayOps {
 
     public static int sum(int[] arr) {
 //sum() takes an array of int, and returns a sum of the individual values in the array.
+    if (arr.length == 0) {
+        return 0;
+    }
     int sum = 0;
     for (int i = 0; i < arr.length; i++) {
         sum += arr[i];
--- Tester.java
+++ Tester.java
@@ -1,3 +1,13 @@
 public class Tester {
-    
+    public static void main(String[] args) {
+        //tests for sum function 
+        int[]arr1 = new int{10,10,10,10,10}; 
+        int[]arr2 = new int{5,10,5}; 
+        int[]arr1 = new int{10}; 
+        int[]arr1 = new int{};
+        System.out.println("Sum function: {10,10,10,10,10} --> 50 | " + sums[arr1]);
+        System.out.println("Sum function: {5,10,5} --> 20" + sums[arr2]);
+        System.out.println("Sum function: {10} --> 10" + sums[arr3]);
+        System.out.println("Sum function: {} --> 0" + sums[arr4]);
+    }
 }
