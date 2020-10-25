7f641d0f25f717a05305e75f195ffc2c6feec5b1
EricShi1255 <eshi20@stuy.edu>
2020-10-24 21:35:06 -0400

    added files

---
+++ ArrayOps.java
@@ -0,0 +1,19 @@
+public class ArrayOps {
+public static int sum(int[] arr) {
+
+}
+public static  int largest(int[]arr) {
+
+}
+public static int[] sumRows(int[][] matrix) {
+
+}
+public static  int[] largestInRows(int[][] matrix) {
+
+}
+public static  int sum(int[][] arr) {
+    
+}
+
+
+}
